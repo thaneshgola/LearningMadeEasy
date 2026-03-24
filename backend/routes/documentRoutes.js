1c36b29de43bf68c37e52b767dcd2aadf3f86dc2
Thanesh Gola <119119055+thaneshgola@users.noreply.github.com>
2026-03-25 01:46:51 +0530

    Completed documentController flow (backendThanesh)
    * checking merging
    
    * merging from backendThanesh branch
    
    * adding one line in frontendPriyanka branch (#3)
    
    Co-authored-by: Priyanka Sharma <priyankash123>
    
    * Completed documentController flow

--- backend/routes/documentRoutes.js
+++ backend/routes/documentRoutes.js
@@ -4,7 +4,6 @@ import {
   getDocuments,
   getDocument,
   deleteDocument,
-  updateDocument,
 } from '../controllers/documentController.js';
 import protect from '../middleware/auth.js';
 import upload from '../config/multer.js';
@@ -18,6 +17,5 @@ router.post('/upload', upload.single('file'), uploadDocument);
 router.get('/', getDocuments);
 router.get('/:id', getDocument);
 router.delete('/:id', deleteDocument);
-router.put('/:id', updateDocument);
 
 export default router;

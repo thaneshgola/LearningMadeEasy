564ffdea7eb6bdb27488d3ad421bf906183c7325
Thanesh Gola <thaneshgola@gmail.com>
2026-03-25 17:56:34 +0530

    Completed aiController and flashcardController flow

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,9 @@ import errorHandler from './middleware/errorHandler.js'
 
 import authRoutes from './routes/authRoutes.js'
 import documentRoutes from './routes/documentRoutes.js'
+import flashcardRoutes from './routes/flashcardRoutes.js'
+import aiRoutes from './routes/aiRoutes.js'
+
 
 // ES6 module __dirname alternative
 const __filename = fileURLToPath(import.meta.url);
@@ -41,6 +44,8 @@ app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 // Routes
 app.use('/api/auth', authRoutes);
 app.use('/api/documents', documentRoutes);
+app.use('/api/flashcards', flashcardRoutes);
+app.use('/api/ai', aiRoutes);
 
 app.use(errorHandler);
 

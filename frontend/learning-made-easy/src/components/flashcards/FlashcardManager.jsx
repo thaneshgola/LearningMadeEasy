d3ade9ebeb0bf53ff9f864a4b889de2207eb72a0
Thanesh Gola <119119055+thaneshgola@users.noreply.github.com>
2026-03-27 16:25:13 +0530

    Complete frontend development till AI Actions Tab, now will start Flashcards tab
    * Defined API services in frontend
    
    * Completed till AI Actions Tab

---
+++ frontend/learning-made-easy/src/components/flashcards/FlashcardManager.jsx
@@ -0,0 +1,26 @@
+import React, { useState, useEffect } from "react";
+import {
+  Plus,
+  ChevronLeft,
+  ChevronRight,
+  Trash2,
+  ArrowLeft,
+  Sparkles,
+  Brain,
+} from "lucide-react";
+import toast from "react-hot-toast";
+import moment from "moment";
+
+import flashcardService from "../../services/flashcardService";
+import aiService from "../../services/aiService";
+import Spinner from "../common/Spinner";
+import Modal from "../common/Modal";
+import Flashcard from "./Flashcard";
+
+const FlashcardManager = ({documentId}) => {
+  return (
+    <div>FlashcardManager</div>
+  )
+}
+
+export default FlashcardManager
